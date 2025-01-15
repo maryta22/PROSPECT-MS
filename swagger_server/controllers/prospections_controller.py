bdef0990387815ac866ccd9eec7a24d1380e55cb
maryta22 <maria.mariaog.rivera@gmail.com>
2025-01-15 13:38:14 -0500

    validacion vendedor

--- swagger_server/controllers/prospections_controller.py
+++ swagger_server/controllers/prospections_controller.py
@@ -3,30 +3,31 @@ from flask import request, jsonify
 from swagger_server.models.prospect_request import ProspectRequest  # noqa: E501
 from ..models import IdStateprospectionsBody
 from ..repositories.prospect_repository import ProspectRepository  # Import repository
+from ..repositories.prospection_repository import ProspectionRepository
 
-prospect_repository = ProspectRepository()
+prospection_repository = ProspectionRepository()
 
 def prospections_get():  # noqa: E501
-    return prospect_repository.get_all_prospections()
+    return prospection_repository.get_all_prospections()
 
 def prospections_id_get(id_):  # noqa: E501
-    return prospect_repository.get_prospection_by_id(id_)
+    return prospection_repository.get_prospection_by_id(id_)
 
 def prospections_id_delete(id_):  # noqa: E501
-    return prospect_repository.delete_prospection_by_id(id_)
+    return prospection_repository.delete_prospection_by_id(id_)
 
 def prospections_id_emails_get(id_):  # noqa: E501
-    return prospect_repository.get_emails_by_prospection_id(id_)
+    return prospection_repository.get_emails_by_prospection_id(id_)
 
 def prospections_id_sales_advisor_get(id_):  # noqa: E501
-    return prospect_repository.get_sales_advisor_by_prospection_id(id_)
+    return prospection_repository.get_sales_advisor_by_prospection_id(id_)
 
 def prospections_post():  # noqa: E501
     if request.is_json:
         try:
             data = request.get_json()
 
-            response, status = prospect_repository.create_prospection(data)
+            response, status = prospection_repository.create_prospection(data)
             return jsonify(response), status
 
         except Exception as e:
@@ -43,7 +44,7 @@ def prospections_id_patch(id_):  # noqa: E501
     if request.is_json:
         try:
             data = request.get_json()
-            response, status = prospect_repository.update_prospection(id_, data)
+            response, status = prospection_repository.update_prospection(id_, data)
             return jsonify(response), status
 
         except Exception as e:
@@ -53,19 +54,19 @@ def prospections_id_patch(id_):  # noqa: E501
         return {"message": "Request body must be JSON."}, 400
 
 def prospects_id_prospections_get(id_):  # noqa: E501
-    return prospect_repository.get_prospections_by_prospect_id(id_)
+    return prospection_repository.get_prospections_by_prospect_id(id_)
 
 def prospections_id_history_get(id_):
-    return prospect_repository.get_prospection_history_with_logs(id_)
+    return prospection_repository.get_prospection_history_with_logs(id_)
 
 def get_state_prospection():  # noqa: E501
-    return prospect_repository.get_state_prospections()
+    return prospection_repository.get_state_prospections()
 
 def prospections_id_state_patch(id_):  # noqa: E501
     if request.is_json:
         try:
             data = request.get_json()  # noqa: E501
-            response, status = prospect_repository.update_prospection_state(id_, data)
+            response, status = prospection_repository.update_prospection_state(id_, data)
             return jsonify(response), status
 
         except Exception as e:
@@ -73,3 +74,17 @@ def prospections_id_state_patch(id_):  # noqa: E501
             return {"message": f"Error processing request: {str(e)}"}, 500
     else:
         return {"message": "Request body must be JSON."}, 400
+
+def prospections_sales_advisor_id_get(advisor_id):  # noqa: E501
+    """
+    Retrieve prospections by sales advisor ID.
+    :param advisor_id: ID of the sales advisor
+    """
+    try:
+        # Usa el repositorio para obtener las prospecciones asociadas al asesor
+        response, status = prospection_repository.get_prospections_by_sales_advisor(advisor_id)
+        return jsonify(response), status
+
+    except Exception as e:
+        logging.error(f"Error retrieving prospections for sales advisor {advisor_id}: {e}")
+        return {"message": f"Error processing request: {str(e)}"}, 500
--- swagger_server/repositories/prospection_repository.py
+++ swagger_server/repositories/prospection_repository.py
@@ -5,7 +5,7 @@ from flask import jsonify
 from sqlalchemy.orm import sessionmaker, joinedload
 from sqlalchemy import create_engine
 from swagger_server.database_models.models import Prospection, Prospect, AcademicProgram, StateProspection, \
-    StateProspectionProspection, Note
+    StateProspectionProspection, Note, ProspectionSalesAdvisor, Email, SalesAdvisor, User
 from dotenv import load_dotenv
 import os
 
@@ -93,3 +93,372 @@ class ProspectionRepository:
             return {"message": f"Error saving note: {str(e)}"}, 500
         finally:
             session.close()
+
+    def create_prospection(self, prospection_data):
+        session = self.Session()
+        try:
+
+            # Validar si ya existe una prospección para el prospecto y programa académico específico
+            existing_prospection = session.query(Prospection).join(StateProspectionProspection).filter(
+                Prospection.id_prospect == prospection_data["prospect_id"],
+                Prospection.id_academic_program == prospection_data["academic_program_id"],
+                StateProspectionProspection.id_state_prospection != 4,
+                StateProspectionProspection.id_state_prospection != 5,
+                StateProspectionProspection.state == 1,
+            ).first()
+            if existing_prospection:
+                return {
+                    "message": "Ya existe una prospección activa con este programa académico."
+                }, 400
+
+            new_prospection = Prospection(
+                id_prospect=prospection_data["prospect_id"],
+                id_academic_program=prospection_data.get("academic_program_id"),
+                date=prospection_data.get("date", datetime.now()),
+                state=prospection_data.get("state", 1),
+                channel=prospection_data.get("channel", "web prospecciones")
+            )
+            session.add(new_prospection)
+            session.flush()
+
+            initial_state = StateProspectionProspection(
+                id_prospection=new_prospection.id,
+                id_state_prospection=1,
+                date=datetime.now(),
+                state=1
+            )
+            session.add(initial_state)
+
+            session.commit()
+
+            return {
+                "message": "Prospección creada exitosamente.",
+                "id": new_prospection.id
+            }, 201
+        except Exception as e:
+            session.rollback()
+            logging.error(f"Error creando la prospección: {e}")
+            return {"message": f"Error creando la prospección: {str(e)}"}, 400
+        finally:
+            session.close()
+
+    def get_all_prospections(self):
+        session = self.Session()
+        try:
+            prospections = session.query(
+                Prospection.id.label("id"),
+                Prospect.id.label("prospect_id"),
+                Prospect.id_number.label("cedula"),
+                Prospect.company.label("company"),
+                Prospection.state.label("state"),
+                Prospection.date.label("date"),
+                AcademicProgram.name.label("program"),
+                Prospection.channel.label("channel"),
+                StateProspection.description.label("prospection_state")  # Estado de gestión actual
+            ).join(Prospect, Prospection.id_prospect == Prospect.id
+                   ).join(AcademicProgram, Prospection.id_academic_program == AcademicProgram.id
+                          ).outerjoin(
+                StateProspectionProspection,
+                StateProspectionProspection.id_prospection == Prospection.id
+            ).outerjoin(
+                StateProspection,
+                StateProspectionProspection.id_state_prospection == StateProspection.id
+            ).filter(StateProspectionProspection.state == 1
+                     ).all()
+
+            result = [
+                {
+                    "id": row.id,
+                    "prospect_id": row.prospect_id,
+                    "cedula": row.cedula,
+                    "company": row.company,
+                    "state": row.state,
+                    "date": row.date.strftime('%Y-%m-%d') if row.date else None,
+                    "program": row.program,
+                    "channel": row.channel,
+                    "prospection_state": row.prospection_state
+                }
+                for row in prospections
+            ]
+            return result, 200
+        except Exception as e:
+            logging.error(f"Error retrieving prospections for table: {e}")
+            return {"message": f"Error retrieving data: {str(e)}"}, 500
+        finally:
+            session.close()
+
+    def get_prospections_by_prospect_id(self, prospect_id):
+        session = self.Session()
+        try:
+            # Realizar la consulta para obtener las prospecciones y el estado del prospecto
+            prospections = session.query(
+                Prospection.id.label("id"),
+                Prospection.date.label("date"),
+                Prospection.state.label("state"),
+                Prospection.channel.label("channel"),
+                AcademicProgram.name.label("program"),
+                Prospect.id.label("prospect_id"),
+                Prospect.company.label("company"),
+                Prospect.id_number.label("cedula"),
+                StateProspection.description.label("prospection_state")  # Estado del prospecto
+            ).join(
+                Prospect, Prospection.id_prospect == Prospect.id
+            ).outerjoin(
+                AcademicProgram, Prospection.id_academic_program == AcademicProgram.id
+            ).outerjoin(
+                StateProspectionProspection, StateProspectionProspection.id_prospection == Prospection.id
+            ).outerjoin(
+                StateProspection, StateProspectionProspection.id_state_prospection == StateProspection.id
+            ).filter(
+                Prospection.id_prospect == prospect_id,
+                StateProspectionProspection.state == 1  # Asegurarse de obtener el estado activo
+            ).all()
+
+            # Si no se encuentran resultados
+            if not prospections:
+                return {"message": "No prospections found for this prospect."}, 404
+
+            # Formatear la respuesta con los datos necesarios
+            result = [
+                {
+                    "id": row.id,
+                    "date": row.date.strftime('%Y-%m-%d') if row.date else None,
+                    "state": row.state,
+                    "channel": row.channel,
+                    "program": row.program,
+                    "prospect_id": row.prospect_id,
+                    "company": row.company,
+                    "cedula": row.cedula,
+                    "prospection_state": row.prospection_state  # Añadir el estado del prospecto
+                }
+                for row in prospections
+            ]
+            return result, 200
+        except Exception as e:
+            logging.error(f"Error retrieving prospections for prospect {prospect_id}: {e}")
+            return {"message": f"Error retrieving prospections: {str(e)}"}, 500
+        finally:
+            session.close()
+
+    def get_prospection_history_with_logs(self, prospection_id):
+        session = self.Session()
+        try:
+            vendedor_historial = session.query(
+                ProspectionSalesAdvisor.date.label("date"),
+                SalesAdvisor.id.label("vendedor_id"),
+                User.first_name.label("vendedor_first_name"),
+                User.last_name.label("vendedor_last_name"),
+                ProspectionSalesAdvisor.state.label("state")
+            ).join(SalesAdvisor, ProspectionSalesAdvisor.id_sales_advisor == SalesAdvisor.id
+                   ).join(User, SalesAdvisor.id_user == User.id
+                          ).filter(ProspectionSalesAdvisor.id_prospection == prospection_id).all()
+
+            estado_historial = session.query(
+                StateProspectionProspection.date.label("date"),
+                StateProspection.description.label("state_description"),
+                StateProspectionProspection.state.label("state")
+            ).join(StateProspection, StateProspectionProspection.id_state_prospection == StateProspection.id
+                   ).filter(StateProspectionProspection.id_prospection == prospection_id).all()
+
+            notas = session.query(
+                Note.date.label("date"),
+                Note.message.label("message"),
+                Note.id.label("note_id")
+            ).filter(Note.id_prospection == prospection_id).all()
+
+            historial = []
+
+            for vendedor in vendedor_historial:
+                log = (
+                    f"Vendedor asignado a {vendedor.vendedor_first_name} {vendedor.vendedor_last_name} "
+                    f"con estado {'activo' if vendedor.state else 'inactivo'}."
+                )
+                historial.append({
+                    "type": "vendedor",
+                    "date": vendedor.date.strftime('%Y-%m-%d %H:%M:%S') if vendedor.date else None,
+                    "log": log,
+                    "details": {
+                        "vendedor_id": vendedor.vendedor_id,
+                        "vendedor_name": f"{vendedor.vendedor_first_name} {vendedor.vendedor_last_name}",
+                        "state": vendedor.state
+                    }
+                })
+
+            for estado in estado_historial:
+                log = (
+                    f"Estado cambiado a '{estado.state_description}'."
+                )
+                historial.append({
+                    "type": "estado",
+                    "date": estado.date.strftime('%Y-%m-%d %H:%M:%S') if estado.date else None,
+                    "log": log,
+                    "details": {
+                        "state_description": estado.state_description,
+                        "state": estado.state
+                    }
+                })
+
+            for nota in notas:
+                log = f"{nota.message}"
+                historial.append({
+                    "type": "nota",
+                    "date": nota.date.strftime('%Y-%m-%d %H:%M:%S') if nota.date else None,
+                    "log": log,
+                    "details": {
+                        "note_id": nota.note_id,
+                        "message": nota.message
+                    }
+                })
+
+            historial.sort(key=lambda x: x["date"], reverse=True)
+
+            return historial, 200
+        except Exception as e:
+            logging.error(f"Error retrieving history for prospection {prospection_id}: {e}")
+            return {"message": f"Error retrieving history: {str(e)}"}, 500
+        finally:
+            session.close()
+
+    def get_state_prospections(self):
+        session = self.Session()
+        try:
+            states = session.query(StateProspection).all()
+            return [state.to_dict() for state in states], 200
+        except Exception as e:
+            logging.error(f"Error retrieving state prospections: {e}")
+            return {"message": f"Error retrieving state prospections: {str(e)}"}, 500
+        finally:
+            session.close()
+
+    def update_prospection_state(self, prospection_id, data):
+        session = self.Session()
+        try:
+            new_state_id = data["new_state_id"]
+            session.query(StateProspectionProspection).filter_by(
+                id_prospection=prospection_id, state=1
+            ).update({"state": 0})
+
+            new_state = StateProspectionProspection(
+                id_prospection=prospection_id,
+                id_state_prospection=new_state_id,
+                date=datetime.now(),
+                state=1
+            )
+            session.add(new_state)
+            session.commit()
+
+            return {"message": "Gestion state updated successfully."}, 200
+        except Exception as e:
+            session.rollback()
+            logging.error(f"Error updating gestion state: {e}")
+            return {"message": f"Error updating gestion state: {str(e)}"}, 500
+        finally:
+            session.close()
+
+    def update_prospection(self, id_, prospection_data):
+        session = self.Session()
+        try:
+            prospection = session.query(Prospection).filter_by(id=id_).first()
+            if not prospection:
+                return {"message": "Prospection not found"}, 404
+
+            for key, value in prospection_data.items():
+                if hasattr(prospection, key):
+                    setattr(prospection, key, value)
+
+            session.commit()
+            return {"message": "Prospection updated successfully."}, 200
+        except Exception as e:
+            session.rollback()
+            logging.error(f"Error updating prospection: {e}")
+            return {"message": f"Error updating prospection: {str(e)}"}, 400
+        finally:
+            session.close()
+
+    def get_emails_by_prospection_id(self, prospection_id):
+        session = self.Session()
+        try:
+            emails = session.query(Email).filter_by(id_prospection=prospection_id).all()
+            if not emails:
+                return {"message": "No emails found for this prospection"}, 404
+
+            return [email.to_dict() for email in emails], 200
+        except Exception as e:
+            logging.error(f"Error retrieving emails: {e}")
+            return {"message": f"Error retrieving emails: {str(e)}"}, 500
+        finally:
+            session.close()
+
+    def get_sales_advisor_by_prospection_id(self, prospection_id):
+        session = self.Session()
+        try:
+            prospection = session.query(ProspectionSalesAdvisor).filter_by(id_prospection=prospection_id).first()
+            if not prospection:
+                return {"message": "No sales advisor found for this prospection"}, 404
+
+            return prospection.sales_advisor.to_dict(), 200
+        except Exception as e:
+            logging.error(f"Error retrieving sales advisor: {e}")
+            return {"message": f"Error retrieving sales advisor: {str(e)}"}, 500
+        finally:
+            session.close()
+
+    def get_prospections_by_sales_advisor(self, sales_advisor_id):
+        session = self.Session()
+        try:
+            # Consultar las prospecciones asociadas al asesor de ventas
+            prospections = session.query(
+                Prospection.id.label("id"),
+                Prospect.id.label("prospect_id"),
+                Prospect.id_number.label("cedula"),
+                Prospect.company.label("company"),
+                Prospection.state.label("state"),
+                Prospection.date.label("date"),
+                AcademicProgram.name.label("program"),
+                Prospection.channel.label("channel"),
+                StateProspection.description.label("prospection_state")  # Estado actual de la prospección
+            ).join(
+                ProspectionSalesAdvisor, Prospection.id == ProspectionSalesAdvisor.id_prospection
+            ).join(
+                SalesAdvisor, ProspectionSalesAdvisor.id_sales_advisor == SalesAdvisor.id
+            ).join(
+                Prospect, Prospection.id_prospect == Prospect.id
+            ).join(
+                AcademicProgram, Prospection.id_academic_program == AcademicProgram.id
+            ).outerjoin(
+                StateProspectionProspection, StateProspectionProspection.id_prospection == Prospection.id
+            ).outerjoin(
+                StateProspection, StateProspectionProspection.id_state_prospection == StateProspection.id
+            ).filter(
+                SalesAdvisor.id == sales_advisor_id,
+                ProspectionSalesAdvisor.state == 1,  # Asegurar que la relación está activa
+                StateProspectionProspection.state == 1  # Estado activo de la prospección
+            ).all()
+
+            # Verificar si no se encontraron prospecciones
+            if not prospections:
+                return {"message": "No se encontraron prospecciones para este asesor de ventas."}, 404
+
+            # Formatear los resultados
+            result = [
+                {
+                    "id": row.id,
+                    "prospect_id": row.prospect_id,
+                    "cedula": row.cedula,
+                    "company": row.company,
+                    "state": row.state,
+                    "date": row.date.strftime('%Y-%m-%d') if row.date else None,
+                    "program": row.program,
+                    "channel": row.channel,
+                    "prospection_state": row.prospection_state
+                }
+                for row in prospections
+            ]
+
+            return result, 200
+        except Exception as e:
+            logging.error(f"Error retrieving prospections for sales advisor {sales_advisor_id}: {e}")
+            return {"message": f"Error retrieving data: {str(e)}"}, 500
+        finally:
+            session.close()
+

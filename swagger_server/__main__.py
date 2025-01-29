9884eaf834ff3062bb3439328d77fb5c0101b3f2
maryta22 <maria.mariaog.rivera@gmail.com>
2025-01-29 01:56:46 -0500

    dockerfile

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -13,7 +13,7 @@ from swagger_server.services.alert import process_and_send_alerts
 def main():
     app = FlaskApp(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
-    CORS(app.app, resources={r"/prospects*": {"origins": "*"}}, supports_credentials=True)
+    CORS(app.app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
     app.add_api('swagger.yaml', arguments={'title': 'PROSPECT-MS'}, pythonic_params=True)
     app.run(host='0.0.0.0', port=2034)
 
